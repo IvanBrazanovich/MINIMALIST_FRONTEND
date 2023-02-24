e51cf17d7ba98d9ac44dbc010231e2711ded758c
Ivan <ivanbrazas@gmail.com>
2023-02-24 10:49:54 -0300

    toggle-sidebar functionality done

--- src/pages/app/NewProject.jsx
+++ src/pages/app/NewProject.jsx
@@ -7,6 +7,10 @@ import { useDispatch } from "react-redux";
 
 const NewProject = () => {
   const [selected, setSelected] = useState([]);
+  const [objSelection, setObjSelection] = useState(() => [
+    { name: "Formularios", component: <Formularios /> },
+    { name: "Texts", component: <Texts /> },
+  ]);
 
   const toggle = (item) => {
     if (selected.includes(item)) {
@@ -22,8 +26,26 @@ const NewProject = () => {
 
       <div className={styles.newProject__flexContainer}>
         <div className={styles.newProject__sidebar}>
-          <Formularios />
-          <Texts />
+          {objSelection?.map((comp) => {
+            return (
+              <div key={comp.name} className={styles.component__toggl}>
+                <h3 onClick={(e) => toggle(comp.name)}>
+                  {comp.name}
+                  <span> {selected.includes(comp.name) ? "-" : "+"}</span>
+                </h3>
+
+                <div
+                  className={`${
+                    selected.includes(comp.name)
+                      ? `${styles.content__active} ${styles.content}`
+                      : styles.content
+                  } `}
+                >
+                  {comp.component}
+                </div>
+              </div>
+            );
+          })}
         </div>
         <div className={styles.newProject__layout}>
           <LayoutPage />
